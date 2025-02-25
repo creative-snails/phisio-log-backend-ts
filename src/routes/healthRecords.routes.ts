fb831d52a3b10a4a28a5218ef45fec6d9ea5e8e7
Alaeddine Jmila <jmila.aladin@gmail.com>
2025-02-25 08:40:11 +0000

    Setup put route to update a new record

--- src/routes/healthRecords.routes.ts
+++ src/routes/healthRecords.routes.ts
@@ -6,7 +6,6 @@ import HealthRecord from "../models/health-record/healthRecord";
 import { HealthRecordType } from "../models/health-record/healthRecordValidation";
 import { validateHealthRecord } from "../services/customValidators";
 import { jsonGen, Message } from "../services/genAI";
-// import transcribe from "../services/transcription";
 
 const router = Router();
 
@@ -39,14 +38,16 @@ const createNewConversation = (): Conversation => {
   return conversation;
 };
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/new-record", async (req: Request, res: Response) => {
   try {
     let newSystemPrompt = "";
     let healthRecord: Partial<HealthRecordType> = {};
-    const conversation = conversations.get(req.body.conversationId) || createNewConversation();
+    const { conversationId, message } = req.body;
+
+    const conversation = conversations.get(conversationId) || createNewConversation();
     conversation.lastAccessed = Date.now();
 
-    conversation.history.push({ role: "user", content: req.body.message });
+    conversation.history.push({ role: "user", content: message });
 
     const generatedJSON = await jsonGen(conversation.history);
     healthRecord = JSON.parse(generatedJSON);
@@ -67,7 +68,7 @@ router.post("/", async (req: Request, res: Response) => {
         conversationId: conversation.id,
         healthRecordId: savedHealthRecord._id,
         message: validationResult.userPrompt,
-        healthRecord: savedHealthRecord,
+        healthRecord,
       });
     } else {
       res.status(200).json({
@@ -79,85 +80,62 @@ router.post("/", async (req: Request, res: Response) => {
                 Don't update single value entries that were already generated if not needed:\n ${JSON.stringify(healthRecord)}`;
 
     if (validationResult.userPrompt) conversation.history.push({ role: "system", content: newSystemPrompt });
-    console.log(conversation);
   } catch (error) {
     res.status(500).json({ message: "Internal server error", error });
   }
 });
 
-const history: Record<string, Message[]> = {
-  "123": [{ role: "system", content: initialSystemPrompt }],
-};
-
-router.post("/temp", async (req: Request, res: Response) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+router.put("/new-record/:healthRecordId", async (req: Request, res: Response): Promise<Response | any> => {
   try {
-    // in the real scenario we'll transcribe and audio file
-    // const audioFilePath = "./transcript-test-10-seconds.wav";
-    // let transcription = ""
-
-    // try {
-    //   transcription = await transcribe(audioFilePath);
-    //   res.send(`Transcription: ${transcription}`);
-    // } catch (error) {
-    //   console.error("Error transcribing audio:", error);
-    //   res.status(500).send("Error transcribing audio");
-    // }
-
-    const conversionId = req.body.conversationId || "123";
-    history[conversionId].push({ role: "user", content: req.body.message });
-    let message = "";
     let newSystemPrompt = "";
+    let healthRecord: Partial<HealthRecordType> = {};
+    const { healthRecordId } = req.params;
+    const { conversationId, message } = req.body;
 
-    const result = await jsonGen(history[conversionId]);
-    let healthRecord: Partial<HealthRecordType> = JSON.parse(result);
+    if (!conversationId || !healthRecordId)
+      return res.status(400).json({ error: "Both conversationId and healthRecordId are required" });
 
-    const validationResult = await validateHealthRecord(healthRecord, history[conversionId]);
+    const conversation = conversations.get(conversationId);
+    if (!conversation) return res.status(404).json({ error: "Conversation not found" });
 
-    if (!validationResult?.success) message = validationResult?.userPrompt ?? "";
+    conversation.history.push({ role: "user", content: message });
+    conversation.lastAccessed = Date.now();
 
-    if (validationResult.success && validationResult.systemPrompt) {
-      message = validationResult?.userPrompt ?? "";
+    const generatedJSON = await jsonGen(conversation.history);
+    healthRecord = JSON.parse(generatedJSON);
+    const validationResult = await validateHealthRecord(healthRecord, conversation.history);
+
+    if (validationResult.userPrompt)
+      conversation.history.push({ role: "assistant", content: validationResult.userPrompt });
+
+    if (validationResult.success) {
       newSystemPrompt += validationResult?.systemPrompt ?? "";
 
-      // result = await jsonGen(history[conversionId]);
-      // healthRecord = JSON.parse(result);
+      const updatedRecord = await HealthRecord.findByIdAndUpdate(healthRecordId, { ...healthRecord }, { new: true });
+
+      if (!updatedRecord) return res.status(404).json({ error: "Health record not found" });
+
+      healthRecord = updatedRecord;
+
+      res.status(200).json({
+        conversationId: conversation.id,
+        message: validationResult.userPrompt,
+        healthRecord,
+      });
+    } else {
+      res.status(200).json({
+        conversationId: conversation.id,
+        message: validationResult.userPrompt,
+      });
     }
 
     newSystemPrompt += `This was your output, update it to iclude the new requirements.
-                Don't update single value entries that were already generated if not needed: ${result}`;
+                Don't update single value entries that were already generated if not needed:\n ${JSON.stringify(healthRecord)}`;
 
-    if (validationResult.success) {
-      let dbHealthRecord: HealthRecordType;
-      try {
-        if (!req.body.healthRecordId) {
-          const savedHealthRecord = new HealthRecord({ ...healthRecord });
-          await savedHealthRecord!.save();
-          console.log("Created new record");
-          dbHealthRecord = savedHealthRecord;
-        } else {
-          const updatedRecord = await HealthRecord.findByIdAndUpdate(
-            req.body.healthRecordId,
-            { ...healthRecord },
-            { new: true }
-          );
-
-          if (!updatedRecord) throw new Error("Health record not found");
-
-          console.log("Updated existing record");
-          dbHealthRecord = updatedRecord;
-        }
-      } catch (error: unknown) {
-        throw new Error(`Failed to ${req.body.healthRecordId ? "update" : "create"} health record: ${error}`);
-      }
-
-      healthRecord = dbHealthRecord;
-    }
-
-    history[conversionId].push({ role: "system", content: newSystemPrompt });
-
-    res.status(200).json({ message, healthRecord });
-  } catch (error: unknown) {
-    res.status(500).json({ error: "Internal server error", message: error });
+    if (validationResult.userPrompt) conversation.history.push({ role: "system", content: newSystemPrompt });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
   }
 });
 
